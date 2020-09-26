90310abb039141dfd112148dac8f6c3ca039c63d
Will8296535 <71922013+Will8296535@users.noreply.github.com>
2020-09-26 16:31:12 +0800

    Add the Unit Test Project

---
+++ UnitTest/UnitTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
+public class Triangle
+{
+    public double thirdAngles(double angleA, double angleB)
+    {
+        return 180 - angleA - angleB;
+    }
+
+    public double area(double sideA, double sideB)
+    {
+        return sideA * sideB * 0.5;
+    }
+}
