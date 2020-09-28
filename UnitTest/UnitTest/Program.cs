db99c8e344f2a848ff145c42c4238a1b331ab0fc
Will8296535 <71922013+Will8296535@users.noreply.github.com>
2020-09-28 22:41:17 +0800

    Add files via upload
    new project

--- UnitTest/UnitTest/Program.cs
+++ UnitTest/UnitTest/Program.cs
@@ -17,6 +17,17 @@ namespace UnitTest
 
 public class Triangle
 {
+
+    private double angleA;
+    private double angleB;
+    private double angleC;
+
+    public Triangle()
+    {
+        angleA = 30;
+        angleB = 60;
+        angleC = 90;
+    }
     public double thirdAngles(double angleA, double angleB)
     {
         return 180 - angleA - angleB;
@@ -25,5 +36,32 @@ public class Triangle
     public double area(double sideA, double sideB)
     {
         return sideA * sideB * 0.5;
+    }
+
+    public double GetAngleA()
+    {
+        return angleA;
+    }
+
+    public double GetAngleB()
+    {
+        return angleB;
+    }
+
+    public string GetType()
+    {
+        if (Math.Max(angleA, angleB) > 90 || angleC > 90)
+        {
+            return "Obtuse triangle";
+        }
+        else if (Math.Max(angleA,angleB) < 90|| angleC < 90)
+        {
+            return "Acute triangle";
+        }
+        else
+        {
+            return "Right triangle";
+        }
+
     }
 }
